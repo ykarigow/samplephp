492fc2871f51bf3b8b28effe50c9da0d1195aed3
princevishwa <35019342+princevishwa@users.noreply.github.com>
2018-01-03 09:38:22 +0530

    version and paragraph

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
 
 <h1>This is my PHP Test Application<h1>
   <p> This is my pawan class of life </p>
+  <p1> hey boss code is ready</p1>
 
 <?php
-echo "Version: 2.2";
+echo "Version: 5.2";
 ?>
 
 </body>
