b021d7d54f2c8016f687193f0f7d74d5b1485ea1
princevishwa <35019342+princevishwa@users.noreply.github.com>
2018-12-01 11:03:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 <h1>This is my PHP Test Application<h1>
   <p> This is my aws bean stalk paas</p>
   <p1> hey boss code is ready</p1>
-  <p> Boom shakalaka bootha sundhari </p>
-  <p> Mass prince  is robo 3 and microbots  and rajini is back </p>
+  <p> shankar direction bad  </p>
+  <p> Mass prince  is robo 3 and microbots  and rajini is back  amy robot is good </p>
 
 <?php
 echo "Version: 5.2";
